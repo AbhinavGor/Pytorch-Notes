34842ad7620a3ada59b86952540f2541e7d5d872
Abhinav Gorantla <abhinav20016@gmail.com>
2026-01-07 15:14:26 -0800

    add transformer block class

---
+++ transformers/blocks.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+from attention import MultiHeadSelfAttention
+
+class MLP(nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+        
+        self.d_model = d_model
+        
+        self.linear1 = nn.Linear(self.d_model, self.d_model)
+        self.activation1 = nn.ReLU()
+        self.linear2 = nn.Linear(self.d_model, self.d_model)
+        
+        
+    def forward(self, x):
+        return self.linear2(self.activation1(self.linear1(x)))
+        
+class TransformerBlock(nn.Module):
+    def __init__(self, d_model, h, ):
+        super().__init__()
+        self.h = h
+        self.d_model = d_model
+        
+        self.mha = MultiHeadSelfAttention(self.d_model, self.h)
+        self.mlp = MLP(self.d_model)
+        
+        self.ln1 = nn.LayerNorm(d_model)
+        self.ln2 = nn.LayerNorm(d_model)
+        
+    def forward(self, x):
+        attention = self.mha(x)
+        
+        norm1 = self.ln1(x+attention)
+        
+        ff = self.mlp(norm1)
+        
+        norm2 = self.ln2(norm1+ff)
+        
+        return norm2
+    
+    
+if __name__ == "__main__":
+    x = torch.randn(128, 1024, 1024)
+    
+    transformer = TransformerBlock(1024, 8)
+    
+    print(transformer(x))
+    
